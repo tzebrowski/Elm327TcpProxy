548d89e31a10c4cbea3824b96cce9668ccc48de2
Tomek Zebrowski <zebrowski.tomek@gmail.com>
2024-01-13 09:00:04 +0100

    feat: Add termination characters and ability to reload config at runtime

--- src/main/java/org/obd/metrics/proxy/ServerHandler.java
+++ src/main/java/org/obd/metrics/proxy/ServerHandler.java
@@ -1,5 +1,6 @@
 package org.obd.metrics.proxy;
 
+import java.io.FileNotFoundException;
 import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,30 +16,32 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelOption;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequiredArgsConstructor
 public class ServerHandler extends ChannelInboundHandlerAdapter {
 
-	private final Settings settings;
+	private static final int TASK_DELAY = 6000;
+	private Settings settings;
 	private volatile Channel adapterChannel;
 	private Map<String, String> overrides = new HashMap<>();
+
+	public ServerHandler(Settings settings) {
+		this.settings = settings;
+	}
 	
 	@Override
 	public void channelActive(ChannelHandlerContext context) {
+
 		loadOverrides();
-		
-		
+		startBackgroundTask();
+
 		final Channel serverChannel = context.channel();
 
 		final Bootstrap bootstrap = new Bootstrap();
 
-		bootstrap.group(serverChannel.eventLoop())
-				 .channel(context.channel().getClass())
-				 .handler(new AdapterInitializer(serverChannel))
-				 .option(ChannelOption.AUTO_READ, true);
+		bootstrap.group(serverChannel.eventLoop()).channel(context.channel().getClass())
+				.handler(new AdapterInitializer(serverChannel)).option(ChannelOption.AUTO_READ, true);
 
 		final Host adapterHost = settings.getAdapter();
 		final ChannelFuture future = bootstrap.connect(adapterHost.getIp(), adapterHost.getPort());
@@ -46,7 +49,20 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 		future.addListener(new WriteAndFlushListener(context));
 	}
 
-
+	private void startBackgroundTask() {
+		new Thread(() -> {
+			while (true) {
+				try {
+					Thread.sleep(TASK_DELAY);
+					settings =  SettingsLoader.settings();
+					loadOverrides();
+				} catch (InterruptedException | FileNotFoundException e) {
+					log.error("Failed to load file",e);
+				}
+			}
+			
+		}).start();
+	}
 
 	@Override
 	public void channelRead(final ChannelHandlerContext context, Object msg) {
@@ -60,12 +76,12 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 
 				if (overrides.containsKey(messageContent)) {
 					final String response = overrides.get(messageContent);
-					final ByteBuf in = Unpooled.copiedBuffer((response + ">").getBytes());
+					final ByteBuf in = Unpooled.copiedBuffer((response + "\r\n>\r\n").getBytes());
 					context.channel().writeAndFlush(in).addListener(writeAndFlushListener);
 				} else {
-					adapterChannel.writeAndFlush(msg).addListener(writeAndFlushListener);
+					final ByteBuf in = Unpooled.copiedBuffer( (((ByteBuf) msg).toString(Charset.defaultCharset()) +  "\r\n>\r\n").getBytes());
+					adapterChannel.writeAndFlush(in).addListener(writeAndFlushListener);
 				}
-
 			}
 		} else {
 			log.error("Channel not active");
@@ -82,8 +98,9 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
 		log.error("Server error failed", cause);
 		Channels.flushAndClose(ctx.channel());
 	}
-	
+
 	private void loadOverrides() {
+		 
 		settings.getOverrides().stream().forEach(o -> {
 			overrides.put(o.getKey() + "\r", o.getValue());
 		});
